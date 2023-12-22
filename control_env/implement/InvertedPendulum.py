d18fe291ac257852c01670f9de36bef6c1d76b2d
soulde <fakesoulde@gmail.com>
2023-12-22 23:01:00 +0800

    update InvertedPendulum to rocket movement situation

--- control_env/implement/InvertedPendulum.py
+++ control_env/implement/InvertedPendulum.py
@@ -1,30 +1,34 @@
 import numpy as np
 from control_env.core.constant import *
 from control_env.core.System import ContinuousSystem, plt
+from control_env.core import g
 
 
 class InvertedPendulum(ContinuousSystem):
-    def __init__(self, M, m, l, init_state, sim_period=0.0001):
+    def __init__(self, M, m, l_half, I, b, init_state, sim_period=0.001):
         super().__init__("InvertedPendulum", init_state, sim_period=sim_period)
         self.M = M
         self.m = m
-        self.l = l
+        self.l_half = l_half
+        self.I = I
+        self.b = b
         self.sim_period = sim_period
         self.sys_time = 0
         self.set_state_name('position', 'velocity', 'angle', 'angular velocity')
-        A = np.array([[0, 1, 0, 0],
-                      [3 * (self.M + self.m) * g / (4 * self.M + self.m) / self.l, 0, 0, 0],
-                      [0, 0, 0, 1],
-                      [-3 * self.m * g / (4 * self.M + self.m), 0, 0, 0]])
-        B = np.array([[0],
-                      [-3 / (4 * self.M + self.m) / self.l],
-                      [0],
-                      [4 / (4 * self.M + self.m)]])
-        C = np.eye(4)
-        D = np.zeros([4, 1])
+        p = I * (M + m) + M * m * l_half ** 2
+        self.A = np.array([[0, 1, 0, 0],
+                           [0, -(I + m * l_half ** 2) * b / p, m ** 2 * g * l_half ** 2 / p, 0],
+                           [0, 0, 0, 1],
+                           [0, -m * l_half * b / p, m * g * l_half * (M + m) / p, 0]])
+        self.B = np.array([[0],
+                           [(I + m * l_half ** 2) / p],
+                           [0],
+                           [m * l_half / p]])
+        self.C = np.eye(4)
+        self.D = np.zeros([4, 1])
 
-        self.sys_fun = lambda x, u, t: A @ self._state + B @ u
-        self.out_fun = lambda x, u, t: C @ self._state + D @ u
+        self.sys_fun = lambda x, u, t: self.A @ self._state + self.B @ u
+        self.out_fun = lambda x, u, t: self.C @ self._state + self.D @ u
 
     def plot(self, title=''):
         plt.rcParams['xtick.direction'] = 'in'
@@ -32,8 +36,11 @@ class InvertedPendulum(ContinuousSystem):
         color_table = []
         t, x = self.history()
         fig = plt.figure(figsize=[12.8, 8])
+
         fig.suptitle(title)
-        ax = fig.add_axes([0.1, 0.1, 0.8, 0.8], aspect=1)
+        ax = fig.add_axes([0.1, 0.1, 0.8, 0.8], aspect='auto')
+        # ax.ylim([0, 100])
+        # ax.yticks(np.arange(0, 100, 10))
         while len(self.state_names) < x.shape[1]:
             self.state_names.append('')
         for i in range(x.shape[1]):
--- demo/inverted_pendulum_control.py
+++ demo/inverted_pendulum_control.py
@@ -1,22 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from control_env.implement.InvertedPendulum import InvertedPendulum
+import tqdm
 
 
 def main():
-    M = 2.
-    m = 0.1
-    l = 0.5
-    K = np.array([[-545.54, -110.16, -380.39, -116.88]])
-    x = np.array([0, 1, 0, 0])
-
-    model = InvertedPendulum(M, m, l, x)
-    for t in range(700):
-        u = -K @ x
-        x = model(u, 0.01)
-
+    M = 20000
+    m = 30000
+    l = 12.5
+    I = 6250000
+    b = 0.15
+    K = np.array([[-0.0316, -56.4784, 9.8164e+05, 1.4600e+06]])
+    x = np.array([0, 0, 0., 0])
+    x_d = np.array([1000, 0, 0., 0])
+    model = InvertedPendulum(M=M, m=m, l_half=l, I=I, b=b, init_state=x)
+    sum_u = 0
+    for t in tqdm.trange(150000):
+        u = -K @ (x - x_d)
+        sum_u += abs(u)
+        # print(u)
+        x = model(u, 0.1)
+    # print(model.A, model.B, model.C, model.D)
     model.plot('Inverted Pendulum')
     # plt.plot(t, x)
+    print(sum_u)
     plt.show()
 
 
